d9a392265a4d2215712cdf40ce71d9c016e23876
Adam Griebel <adgriebe@esu10.org>
2023-03-19 23:56:22 -0500

    cleaning it up

--- netlify/functions/get_movies.js
+++ netlify/functions/get_movies.js
@@ -1,35 +1,13 @@
-﻿////const { MongoClient } = require("mongodb");
+﻿const { MongoClient } = require("mongodb");
 
-////const mongoClient = new MongoClient(process.env.MONGODB_URI);
-
-////const clientPromise = mongoClient.connect();
-
-////const handler = async (event) => {
-////    try {
-////        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
-////        const collection = database.collection(process.env.MONGODB_COLLECTION);
-////        const results = await collection.find({}).limit(10).toArray();
-////        return {
-////            statusCode: 200,
-////            body: JSON.stringify(results),
-////        }
-////    } catch (error) {
-////        return { statusCode: 500, body: error.toString() }
-////    }
-////}
-
-module.exports = { handler }
-
-const { MongoClient } = require("mongodb");
-
-const mongoClient = new MongoClient("mongodb+srv://AdminDB:2c6YQWKRZnD1eg40@cluster0.gsk7aru.mongodb.net/?retryWrites=true&w=majority");
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
 const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
-        const database = (await clientPromise).db("sample_mflix");
-        const collection = database.collection("movies");
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
         const results = await collection.find({}).limit(10).toArray();
         return {
             statusCode: 200,
@@ -39,8 +17,3 @@ const handler = async (event) => {
         return { statusCode: 500, body: error.toString() }
     }
 }
-
-mongoClient.close();
-
-
-module.exports = { handler }
